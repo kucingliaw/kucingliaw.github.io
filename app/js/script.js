856c656ee1d92fa87fa77412a5949a2750e27b03
Wawal <wawal.wiguna@gmail.com>
2022-08-23 04:17:36 +0700

    Add contact section.

--- app/js/script.js
+++ app/js/script.js
@@ -12,3 +12,13 @@ btn.addEventListener('click', () => {
     menu.classList.toggle('fade-in');
     menu.classList.toggle('fade-out');
 });
+
+// Scroll Up
+
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-Up class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
